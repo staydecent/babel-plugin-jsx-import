51613c2895fc7393dffcbbfb1c7ff3584d338379
Adrian Unger <dev@staydecent.ca>
2017-01-23 12:44:20 -0800

    Fix export (browserify) and standard format.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-export default function ({ types: t }) {
+module.exports = function ({types: t}) {
   return {
     visitor: {
-      JSXOpeningElement(path, { file }) {
-        file.set('hasJSX', true);
+      JSXOpeningElement (path, {file}) {
+        file.set('hasJSX', true)
       },
 
       Program: {
-        enter(path, { file }) {
-          file.set('hasJSX', false);
+        enter (path, {file}) {
+          file.set('hasJSX', false)
         },
 
-        exit({ node, scope }, {
+        exit ({node, scope}, {
           file,
           opts: {
             identifier = 'React',
@@ -18,16 +18,16 @@ export default function ({ types: t }) {
           }
         }) {
           if (!(file.get('hasJSX') && !scope.hasBinding(identifier))) {
-            return;
+            return
           }
 
           const jsxImportDeclaration = t.importDeclaration([
-            t.importDefaultSpecifier(t.identifier(identifier)),
-          ], t.stringLiteral(moduleName));
+            t.importDefaultSpecifier(t.identifier(identifier))
+          ], t.stringLiteral(moduleName))
 
-          node.body.unshift(jsxImportDeclaration);
-        },
-      },
-    },
-  };
+          node.body.unshift(jsxImportDeclaration)
+        }
+      }
+    }
+  }
 }

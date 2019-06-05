f0a2a16bba893f90e216eaeec01be62a26853886
Adrian Unger <dev@staydecent.ca>
2019-06-05 15:41:04 -0700

    Add support for named imports
    Ex. `import { h } from 'preact'`

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
-module.exports = function ({types: t}) {
+module.exports = function ({ types: t }) {
   return {
     visitor: {
-      JSXOpeningElement (path, {file}) {
+      JSXOpeningElement (path, { file }) {
         file.set('hasJSX', true)
       },
 
       Program: {
-        enter (path, {file}) {
+        enter (path, { file }) {
           file.set('hasJSX', false)
         },
 
-        exit ({node, scope}, {
+        exit ({ node, scope }, {
           file,
           opts: {
             identifier = 'React',
-            moduleName = 'react'
+            moduleName = 'react',
+            isNamed = false
           }
         }) {
           if (!(file.get('hasJSX') && !scope.hasBinding(identifier))) {
             return
           }
 
-          const jsxImportDeclaration = t.importDeclaration([
-            t.importDefaultSpecifier(t.identifier(identifier))
-          ], t.stringLiteral(moduleName))
+          const dec = t.importDeclaration
+
+          const jsxImportDeclaration = isNamed
+            ? dec([t.ImportSpecifier(t.identifier(identifier), t.identifier(identifier))], t.stringLiteral(moduleName))
+            : dec([t.importDefaultSpecifier(t.identifier(identifier))], t.stringLiteral(moduleName))
 
           node.body.unshift(jsxImportDeclaration)
         }
